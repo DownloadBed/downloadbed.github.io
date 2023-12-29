b9e04fd0472a0f9b94dbfa19e54e2e51df9eeae3
theblindkarp <nicknorman0206@gmail.com>
2023-12-28 23:28:27 -0500

    home page finished

--- js/index.js
+++ js/index.js
@@ -6,33 +6,97 @@
     MODIFICATION HISTORY:
     25 Nov 2023 (NPN)
     23 Dec 2023 (NPN)
+    28 Dec 2023 (NPN)
 */
 
 var currentGames = [
-    ["Be The Pin (Remastered)", ["PC", "Mobile", "itch.io", "Play Store"], "./resources/default-thumbnail.png", ""],
-    ["Quest of the Millionth Trade", ["PC", "Steam", "itch.io"], "./resources/default-thumbnail.png", ""],
-    ["Mighty Sudoku", ["Mobile", "App Store", "Play Store"], "./resources/default-thumbnail.png", ""]
+    ["be-the-pin", ["PC", "Mobile", "itch.io", "Play Store"]],
+    ["trade-quest", ["PC", "Steam", "itch.io"]],
+    ["mighty-sudoku", ["Mobile", "App Store", "Play Store"]]
 ];
 
 //function when options is selected
+var isTagSelected = false;
+var tagSelected = "";
+var optionSelected = null;
+
+function setOptionDecor(option) {
+    option.classList.add("shop-selected");
+}
+
+function removeOptionDecor(option) {
+    option.classList.remove("shop-selected");
+}
+
+function showGame(game) {
+    game.style.display = "block";
+}
+
+function hideGame(game) {
+    game.style.display = "none";
+}
+
+function selectTag(tag, option) {
+    //set decor
+    setOptionDecor(option);
+    //save for future use
+    isTagSelected = true;
+    tagSelected = tag;
+    optionSelected = option;
+
+    //for each game
+    for (var i = 0; i < currentGames.length; i++) {
+        //hide game
+        var tempGame = document.getElementById(currentGames[i][0]);
+        hideGame(tempGame);
+    }
+    
+    //for each game
+    for (var i = 0; i < currentGames.length; i++) {
+        //if tag is present
+        if (currentGames[i][1].includes(tag)) {
+            //show game
+            var tempGame = document.getElementById(currentGames[i][0]);
+            showGame(tempGame);
+        }
+    }
+}
+
+function deselectTag() {
+    //remove decor
+    removeOptionDecor(optionSelected);
+    isTagSelected = false;
+    tagSelected = "";
+    optionSelected = null;
+
+    //show all games
+    for (var i = 0; i < currentGames.length; i++) {
+        //show game
+        var tempGame = document.getElementById(currentGames[i][0]);
+        showGame(tempGame);
+    }
+}
+
 function selectShopTag(e) {
     var target = e.target;
+    var tag = target.textContent.toString();
+    //makes sure list element is li/text
     if(target instanceof HTMLLIElement) {
-        //bold, underline, and add "X " on word
-        target.classList.add("shop-selected");
-        //remove all games by display none
-
-        //for each game stored
-        for (var i = 0; i < currentGames.length; i++) {
-            //if value(s) is same as button pressed
-            if (currentGames[i][1].includes(target.textContent.toString())) {
-                //get game gallery child, then dinsplay none, set bool, etc (look at board)
+        if (isTagSelected == false) {
+            //select Tag
+            selectTag(tag,target);
+        } else {
+            //tag is selected
+            //if tag is the same
+            if (tagSelected == tag) {
+                deselectTag();
             } else {
-                console.log("not in");
+                //else tag isnt the same
+                //deselect old
+                deselectTag();
+                //select new
+                selectTag(tag, target);
             }
-                //append game
-            //else
-                //do nothing
         }
     }
     
